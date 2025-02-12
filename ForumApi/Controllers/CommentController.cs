6f6de4e2475d013fba784f49749b1ba1d2728036
Kxuma <fidrusa@kkszki.hu>
2025-02-12 10:21:28 +0100

    Comment controller is ready

---
+++ ForumApi/Controllers/CommentController.cs
@@ -0,0 +1,46 @@
+﻿using ForumApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static ForumApi.Models.Dto;
+
+namespace ForumApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase {
+        private readonly ForumContext _forumContext;
+
+        public CommentController(ForumContext forumContext) {
+            _forumContext = forumContext;
+        }
+
+
+        // POST method
+
+        [HttpPost]
+        public async Task<ActionResult<Comment>> Post(CreateCommentDto createCommentDto) {
+            var comment = new Comment {
+                Id = createCommentDto.Id,
+                UId = createCommentDto.UId,
+                TId = createCommentDto.TId,
+                Text = createCommentDto.Text
+            };
+
+            _forumContext.Comments.Add(comment);
+            await _forumContext.SaveChangesAsync();
+
+            return StatusCode(201, comment);
+        }
+
+
+        // GET method
+
+        [HttpGet]
+        public async Task<ActionResult<List<Comment>>> GetAllComments() {
+            var comments = await _forumContext.Comments.ToListAsync();
+            return Ok(comments);
+        }
+    }
+}
---
+++ ForumApi/Models/Dto.cs
@@ -0,0 +1,6 @@
+﻿namespace ForumApi.Models {
+    public class Dto 
+    {
+        public record CreateCommentDto(int Id, int UId, int TId, string Text);
+    }
+}
